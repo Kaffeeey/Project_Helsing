6b98d1b5f3a59bc47976f98944bcb30e7cb1f448
Scaers <42579094+Scaers@users.noreply.github.com>
2021-04-17 15:39:06 +0200

    Revert "Merge branch 'main' of https://github.com/Kaffeeey/Project_Helsing into main"
    This reverts commit 969500faf19cd4dfbcf091fcd8eaa9641a41d9af, reversing
    changes made to 2645959525a66246083d574ffd83aed03d4b3691.

--- Source/Project_Helsing/Project_HelsingGameMode.cpp
+++ Source/Project_Helsing/Project_HelsingGameMode.cpp
@@ -9,7 +9,7 @@ AProject_HelsingGameMode::AProject_HelsingGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FPSChar"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
