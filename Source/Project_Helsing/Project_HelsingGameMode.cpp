9bef9b6056c07a0f3182522f64d9d6f617d852a7
FlorianTrippe <fltrippe@stud.mediadesign.de>
2021-04-17 15:10:42 +0200

    GameMode

--- Source/Project_Helsing/Project_HelsingGameMode.cpp
+++ Source/Project_Helsing/Project_HelsingGameMode.cpp
@@ -9,7 +9,7 @@ AProject_HelsingGameMode::AProject_HelsingGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FPSChar"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
